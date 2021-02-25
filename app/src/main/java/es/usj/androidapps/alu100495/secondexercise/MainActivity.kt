36393c93c29011fac09d25c1f55f21b076183977
TestAndroid <5jitibor@gmail.com>
2021-02-25 19:00:52 +0100

    Version 1.0

---
+++ app/src/main/java/es/usj/androidapps/alu100495/secondexercise/MainActivity.kt
@@ -0,0 +1,11 @@
+package es.usj.androidapps.alu100495.secondexercise
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
