fa7047f98d3c05c794fbc7194fca867a565e674c
TestAndroid <5jitibor@gmail.com>
2021-02-25 19:17:32 +0100

    master: onCreate, onStart, on Restart Implement

--- app/src/main/java/es/usj/androidapps/alu100495/secondexercise/MainActivity.kt
+++ app/src/main/java/es/usj/androidapps/alu100495/secondexercise/MainActivity.kt
@@ -2,10 +2,22 @@ package es.usj.androidapps.alu100495.secondexercise
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Toast.makeText(this, R.string.on_create,Toast.LENGTH_SHORT)
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Toast.makeText(this, R.string.on_restart,Toast.LENGTH_SHORT)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Toast.makeText(this, R.string.on_start,Toast.LENGTH_SHORT)
     }
 }
